750ee6546515dfe108c9a7538cad1a86192edd8a
zhuhua <zhuhua@zhuhua-H61M-DS2>
2014-12-24 17:46:41 +0800

    Change struct

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ Created on 2013-3-26
 '''
 import os
 
-installed_apps = ['account']
+installed_apps = ['api', 'backend']
 
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 static_dir = os.path.join(os.path.dirname(__file__), "static")
