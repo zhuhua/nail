e62ba198f29814643e31283cc102a693a7cba4dc
zhuhua <zhuhua@zhuhua-H61M-DS2>
2015-01-08 18:04:14 +0800

    Update SQL

--- scripts/nail.sql
+++ scripts/nail.sql
@@ -10,26 +10,204 @@ Target Server Type    : MYSQL
 Target Server Version : 50540
 File Encoding         : 65001
 
-Date: 2014-12-23 17:30:55
+Date: 2015-01-08 18:03:36
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for `account`
+-- Table structure for artisan
 -- ----------------------------
-DROP TABLE IF EXISTS `account`;
-CREATE TABLE `account` (
-  `id` varchar(32) NOT NULL,
+DROP TABLE IF EXISTS `artisan`;
+CREATE TABLE `artisan` (
+  `id` char(32) NOT NULL COMMENT 'uuid',
+  `name` varchar(100) NOT NULL COMMENT '手艺人-名称',
+  `password` varchar(255) NOT NULL,
+  `gender` bit(1) NOT NULL,
+  `avatar` varchar(255) NOT NULL COMMENT '头像',
+  `level` tinyint(4) NOT NULL DEFAULT '0' COMMENT '等级（0-15）',
+  `avg_price` float NOT NULL,
+  `cert_pop` bit(1) NOT NULL DEFAULT b'0' COMMENT '明星美甲师认证',
+  `cert_pro` bit(1) NOT NULL DEFAULT b'0' COMMENT '高级职业美甲师认证',
+  `brief` varchar(1000) NOT NULL COMMENT '自我介绍',
+  `create_time` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='手艺人';
+
+-- ----------------------------
+-- Records of artisan
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for category
+-- ----------------------------
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL COMMENT '类型（美甲，美足，美睫，护理）',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='作品分类';
+
+-- ----------------------------
+-- Records of category
+-- ----------------------------
+INSERT INTO `category` VALUES ('1', '美甲', '0000-00-00 00:00:00');
+INSERT INTO `category` VALUES ('2', '美睫', '0000-00-00 00:00:00');
+INSERT INTO `category` VALUES ('3', '手足护理', '0000-00-00 00:00:00');
+INSERT INTO `category` VALUES ('4', '空气净化', '0000-00-00 00:00:00');
+
+-- ----------------------------
+-- Table structure for counts
+-- ----------------------------
+DROP TABLE IF EXISTS `counts`;
+CREATE TABLE `counts` (
+  `id` bigint(20) NOT NULL,
+  `object_id` char(32) NOT NULL,
+  `amount` int(11) NOT NULL DEFAULT '0',
+  `type` tinyint(4) NOT NULL COMMENT '类型（接单数，）',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='通用计数';
+
+-- ----------------------------
+-- Records of counts
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for faverite
+-- ----------------------------
+DROP TABLE IF EXISTS `faverite`;
+CREATE TABLE `faverite` (
+  `id` bigint(20) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `object_id` char(32) NOT NULL,
+  `create_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  `is_valid` bit(1) NOT NULL DEFAULT b'1',
+  `type` int(11) NOT NULL COMMENT '0, 收藏手艺人 1, 收藏样品',
+  PRIMARY KEY (`id`),
+  KEY `fk_user_id` (`user_id`),
+  CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `account` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='收藏关系';
+
+-- ----------------------------
+-- Records of faverite
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for gallery
+-- ----------------------------
+DROP TABLE IF EXISTS `gallery`;
+CREATE TABLE `gallery` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `object_id` varchar(255) NOT NULL,
+  `type` tinyint(4) NOT NULL COMMENT '图片类型，0.手艺人相册，1.样品图片，3.作品图片（订单完成实际效果图上）',
+  `url` varchar(500) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='通用相册';
+
+-- ----------------------------
+-- Records of gallery
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for login_token
+-- ----------------------------
+DROP TABLE IF EXISTS `login_token`;
+CREATE TABLE `login_token` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `token` varchar(32) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `expire` bigint(20) NOT NULL,
+  `last_login` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='登录令牌';
+
+-- ----------------------------
+-- Records of login_token
+-- ----------------------------
+INSERT INTO `login_token` VALUES ('1', 'c04ef209d3ed4e15b2554485f0650d0f', '1', '1422004202', '2014-12-24 17:10:01');
+
+-- ----------------------------
+-- Table structure for sample
+-- ----------------------------
+DROP TABLE IF EXISTS `sample`;
+CREATE TABLE `sample` (
+  `id` char(32) NOT NULL COMMENT 'uuid',
+  `name` varchar(100) NOT NULL COMMENT '方案-名称',
+  `price` float NOT NULL,
+  `tag_price` float NOT NULL COMMENT '介绍',
+  `sale` smallint(6) NOT NULL,
+  `brief` varchar(1000) NOT NULL,
+  `category_id` bigint(20) NOT NULL COMMENT '分类',
+  `artisan_id` char(32) NOT NULL COMMENT '提供服务的手艺人',
+  `is_valid` bit(1) DEFAULT b'1',
+  PRIMARY KEY (`id`),
+  KEY `fk_category_id` (`category_id`),
+  KEY `dj_atusab_id` (`artisan_id`),
+  CONSTRAINT `dj_atusab_id` FOREIGN KEY (`artisan_id`) REFERENCES `artisan` (`id`),
+  CONSTRAINT `fk_category_id` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='美甲师提供的作品方案';
+
+-- ----------------------------
+-- Records of sample
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sample_tag
+-- ----------------------------
+DROP TABLE IF EXISTS `sample_tag`;
+CREATE TABLE `sample_tag` (
+  `id` int(11) NOT NULL,
+  `sample_id` char(32) NOT NULL,
+  `tag_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_sample_id` (`sample_id`),
+  CONSTRAINT `fk_sample_id` FOREIGN KEY (`sample_id`) REFERENCES `sample` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='作品标签';
+
+-- ----------------------------
+-- Records of sample_tag
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tag
+-- ----------------------------
+DROP TABLE IF EXISTS `tag`;
+CREATE TABLE `tag` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL COMMENT '标签（圣诞节，日韩，纯色，新娘，法式，创意，彩绘，糖果）',
+  `is_valid` bit(1) NOT NULL DEFAULT b'1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COMMENT='标签';
+
+-- ----------------------------
+-- Records of tag
+-- ----------------------------
+INSERT INTO `tag` VALUES ('1', '圣诞节', '');
+INSERT INTO `tag` VALUES ('2', '特价款', '');
+INSERT INTO `tag` VALUES ('3', '糖果 ', '');
+INSERT INTO `tag` VALUES ('4', '创意 ', '');
+INSERT INTO `tag` VALUES ('5', '彩绘 ', '');
+INSERT INTO `tag` VALUES ('6', '日韩 ', '');
+INSERT INTO `tag` VALUES ('7', '纯色 ', '');
+INSERT INTO `tag` VALUES ('8', '新娘 ', '');
+INSERT INTO `tag` VALUES ('9', '法式 ', '');
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
   `mobile` varchar(16) NOT NULL,
   `password` varchar(48) NOT NULL,
   `nick` varchar(24) DEFAULT NULL,
   `avatar` varchar(255) DEFAULT NULL,
   `reg_time` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='用户';
 
 -- ----------------------------
--- Records of account
+-- Records of users
 -- ----------------------------
-INSERT INTO `account` VALUES ('5524a72a504f40aa86f95ae43bedc14f', '13812345678', '7c4a8d09ca3762af61e59520943dc26494f8941b', '', '', '2014-12-23 17:22:51');
+INSERT INTO `users` VALUES ('1', '13812345678', '7c4a8d09ca3762af61e59520943dc26494f8941b', '', '', '2014-12-24 17:02:44');
