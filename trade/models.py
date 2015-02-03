d68322e226c3e2573733aed8a1a19004d36900d0
lostsquirrel <songli1988@gmail.com>
2015-02-03 18:12:07 +0800

    add service order list for admin

--- trade/models.py
+++ trade/models.py
@@ -7,6 +7,7 @@ Created on Jan 28, 2015
 from datetime import datetime
 from simpletor import torndb
 
+    
 class Order(torndb.Row):
     '''
     订单
@@ -20,7 +21,7 @@ class Order(torndb.Row):
         self.title = None
         self.order_no = None
         self.trade_no = None
-        self.status = None
+        self.status = 0
         self.create_time = datetime.now()
         self.update_time = datetime.now()
         self.display_buyer = True
@@ -55,66 +56,161 @@ class OrderDAO:
         return sql
     
     @torndb.get
-    def count_orders_by_seller(self, artisan_id):
+    def count_orders_by_seller(self, artisan_id, status = None):
         sql = '''
-        SELECT COUNT(id) FROM order o WHERE o.artisan_id = %s AND o.display_seller = true;
+        SELECT COUNT(id) FROM order o 
+        WHERE o.artisan_id = %s AND o.status = %s AND o.display_seller = true;
         '''
+        if status == None:
+            sql = '''
+            SELECT COUNT(id) FROM order o 
+            WHERE o.artisan_id = %s AND o.display_seller = true;
+            '''
         return sql
     
     @torndb.select
-    def find_orders_by_seller(self, artisan_id, max_results, first_result):
+    def find_orders_by_seller(self, artisan_id, status = None, max_results, first_result):
         sql = '''
-        SELECT * FROM order o WHERE o.artisan_id = %s AND o.display_seller = true LIMIT %s OFFSET %s;
+        SELECT * FROM order o 
+        WHERE o.artisan_id = %s AND o.status = %s AND o.display_seller = true 
+        LIMIT %s OFFSET %s;
         '''
+        if status == None:
+            sql = '''
+            SELECT * FROM order o 
+            WHERE o.artisan_id = %s AND o.display_seller = true 
+            LIMIT %s OFFSET %s;
+            '''
+            
         return sql
     
     @torndb.get
-    def count_orders_by_seller_status(self, artisan_id, status):
+    def count_orders_by_buyer(self, artisan_id, status = None):
         sql = '''
-        SELECT COUNT(id) FROM order o WHERE o.artisan_id = %s AND o.status = %s AND o.display_seller = true;
+        SELECT COUNT(id) FROM order o 
+        WHERE o.user_id = %s AND o.status = %s AND o.display_buyer = true;
         '''
+        if status == None:
+            sql = '''
+            SELECT COUNT(id) FROM order o 
+            WHERE o.user_id = %s AND o.display_buyer = true;
+            '''
+            
         return sql
     
     @torndb.select
-    def find_orders_by_seller_status(self, artisan_id, status, max_results, first_result):
+    def find_orders_by_buyer(self, user_id, status = None, max_results, first_result):
         sql = '''
-        SELECT * FROM order o WHERE o.artisan_id = %s AND o.status = %s AND o.display_seller = true LIMIT %s OFFSET %s;
+        SELECT * FROM order o 
+        WHERE o.user_id = %s AND o.status = %s AND o.display_buyer = true 
+        LIMIT %s OFFSET %s;
         '''
-        return sql
-    
-    @torndb.get
-    def count_orders_by_buyer(self, user_id):
-        sql = '''
-        SELECT COUNT(id) FROM order o WHERE o.user_id = %s AND o.display_buyer = true;
-        '''
-        return sql
-    
-    @torndb.select
-    def find_orders_by_buyer(self, user_id, max_results, first_result):
-        sql = '''
-        SELECT * FROM order o WHERE o.user_id = %s AND o.display_buyer = true LIMIT %s OFFSET %s;
-        '''
-        return sql
-    
-    @torndb.get
-    def count_orders_by_buyer_status(self, artisan_id, status):
-        sql = '''
-        SELECT COUNT(id) FROM order o WHERE o.user_id = %s AND o.status = %s AND o.display_buyer = true;
-        '''
+        if status == None:
+            sql = '''
+            SELECT * FROM order o 
+            WHERE o.user_id = %s AND o.display_buyer = true LIMIT %s OFFSET %s;
+            '''
         return sql
     
     @torndb.select
-    def find_orders_by_buyer_status(self, user_id, status, max_results, first_result):
+    def count_orders_by_admin(self, buyer = None, seller = None, status = None, 
+                              start_date = None, end_date = None):
+        params = list()
+        has_params = False
+        if buyer == None:
+            params.append('%s')
+        else:
+            params.append('o.user_id = %s')
+            has_params = True
+            
+        if seller == None:
+            params.append('%s')
+        else:
+            params.append('o.artisan_id = %s')
+            has_params = True
+        
+        if status == None:
+            params.append('%s')
+        else:
+            params.append('o.status = %s')
+            has_params = True
+            
+        if start_date == None:
+            params.append('%s')
+        else:
+            params.append('o.create_time > %s')
+            has_params = True
+            
+        if start_date == None:
+            params.append('%s')
+        else:
+            params.append('o.create_time < %s')
+            has_params = True
+            
         sql = '''
-        SELECT * FROM order o WHERE o.user_id = %s AND o.status = %s AND o.display_buyer = true LIMIT %s OFFSET %s;
+        SELECT COUNT(id) FROM order o;
         '''
+        if has_params:
+            sql = '%s WHERE %s' % (sql, ' '.join(params))
         return sql
     
     @torndb.select
-    def all(self):
+    def find_orders_by_admin(self, buyer = None, seller = None, status = None, 
+                              start_date = None, end_date = None, max_results, first_result):
+        params = list()
+        has_params = False
+        if buyer == None:
+            params.append('%s')
+        else:
+            params.append('o.user_id = %s')
+            has_params = True
+            
+        if seller == None:
+            params.append('%s')
+        else:
+            params.append('o.artisan_id = %s')
+            has_params = True
+        
+        if status == None:
+            params.append('%s')
+        else:
+            params.append('o.status = %s')
+            has_params = True
+            
+        if start_date == None:
+            params.append('%s')
+        else:
+            params.append('o.create_time > %s')
+            has_params = True
+            
+        if start_date == None:
+            params.append('%s')
+        else:
+            params.append('o.create_time < %s')
+            has_params = True
+            
         sql = '''
-        SELECT * FROM category;
+        SELECT COUNT(id) FROM order o;
         '''
+        if has_params:
+            sql = '%s WHERE %s LIMIT %%s OFFSET %%s' % (sql, ' '.join(params))
+        return sql
+    
+    def save(self, **order):
+        sql = '''
+        INSERT INTO orders (user_id, buyer_name, address, 
+        telephone, title, order_no, trade_no, status, create_time, 
+        update_time, display_buyer, display_seller, is_reviewed, 
+        artisan_id, artisan_name, sample_id, sample_name, cover, 
+        tag_price) 
+        VALUES ('%(user_id)s', '%(buyer_name)s', '%(address)s', 
+        '%(telephone)s', '%(title)s', '%(order_no)s', 
+        '%(trade_no)s', '%(status)s', '%(create_time)s', 
+        '%(update_time)s', '%(display_buyer)s', '%(display_seller)s', 
+        '%(is_reviewed)s', '%(artisan_id)s', '%(artisan_name)s', 
+        '%(sample_id)s', '%(sample_name)s', '%(cover)s', '%(tag_price)s', '%(price)s');
+        '''
+        
         return sql
     
 orderDAO = OrderDAO()
--- trade/services.py
+++ trade/services.py
@@ -7,17 +7,21 @@ Created on Jan 28, 2015
 from simpletor.torndb import transactional
 from simpletor.application import AppError
 # from simpletor.tornsolr import index, connect
-from simpletor.utils import validate_utils
+from simpletor.utils import validate_utils, generate_order_no
 from common import services as common_serv
+from artisan import services as artisan_serv
+from sample import services as sample_serv
+from user import services as user_serv
+
 import models
 import settings
-import datetime
+from datetime import date, datetime
 
 order_status_description = ('未支付','已支付', '出发', '到达', '完成')
 order_operate_description = ('pay', 'send', 'arrived', 'finish')
 
 def appointment_status(artisan_id, appt_date):
-    if appt_date < datetime.date.today():
+    if appt_date < date.today():
         raise AppError("超出可预约时间范围")
     appts = models.appointmentDAO.find(artisan_id, appt_date);
     appt_hours = list()
@@ -44,33 +48,75 @@ def close_appointment(artisan_id, appt_date, appt_hour):
     appt.appt_hour = appt_hour
     
     models.appointmentDAO.save(**appt)
+
+@transactional
+def create_order(user_id, sample_id, order_from, address, appt_date, appt_hour):
+    if appt_date < date.today() or appt_hour < settings.appointmentRange[0] or appt_hour > settings.appointmentRange[1]:
+        raise AppError("超出可预约时间范围")
     
+    user = user_serv.get_profile(user_id)
+    sample = sample_serv.get_sample(sample_id)
+    if sample == None:
+        raise AppError("作品不存在(id:%s)" % (sample_id))
+    
+    artisan_id = sample.artisan_id
+    artisan = artisan_serv.get_artisan(artisan_id)
+    if artisan == None:
+        raise AppError("手艺人不存在(id:%s)" % (artisan_id))
+    order = models.Order()
+    order.address = address
+    order.artisan_id = sample.artisan_id
+    order.artisan_name = artisan.name
+    order.buyer_name = user.nick
+    order.cover = sample.images[0]
+    order.create_time = datetime.now()
+    order.display_buyer = True
+    order.display_seller = True
+    order.is_reviewed = False
+    order_no = generate_order_no()
+    order.order_no = order_no
+    order.price = sample.price
+    order.sample_id = sample_id
+    order.sample_name = sample.name
+    order.status = 0;
+    order.tag_price = sample.tag_price
+    order.telephone = user.mobile
+    order.title = sample.name
+    order.trade_no = generate_order_no()
+    order.update_time = datetime.now()
+    order.user_id = user_id
+    
+    models.orderDAO.save(**order)
+    
+    return get_order_orderno(order_no)
+    
+def get_order(order_id):
+    return models.orderDAO.find(order_id)
+
+def get_order_orderno(order_no):
+    return models.orderDAO.find_by_order_no(order_no)
+
 def seller_orders(artisan_id, status = None, page = 1, page_size = 10):
-    orders = None
-    total = 0
     first_result = (page - 1) * page_size
-    if status == None:
-        # 卖家全部订单
-        total = models.orderDAO.count_orders_by_seller(artisan_id)
-        orders = models.orderDAO.find_orders_by_seller(artisan_id, page_size, first_result)
-    else:
-        # 卖家 状态订单
-        total = models.orderDAO.count_orders_by_seller_status(artisan_id, status)
-        orders = models.orderDAO.find_orders_by_seller_status(artisan_id, status, page_size, first_result)
+        # 卖家订单
+    total = models.orderDAO.count_orders_by_seller(artisan_id)
+    orders = models.orderDAO.find_orders_by_seller(artisan_id, page_size, first_result)
     
     return orders, total
 
 def buyer_orders(user_id, status = None, page = 1, page_size = 10):
-    orders = None
-    total = 0
     first_result = (page - 1) * page_size
-    if status == None:
-        # 买家全部订单
-        total = models.orderDAO.count_orders_by_buyer(user_id)
-        orders = models.orderDAO.find_orders_by_buyer(user_id, page_size, first_result)
-    else:
         # 买家 状态订单
-        total = models.orderDAO.count_orders_by_buyer_status(user_id, status)
-        orders = models.orderDAO.find_orders_by_buyer_status(user_id, status, page_size, first_result)
+    total = models.orderDAO.count_orders_by_buyer(user_id, status)
+    orders = models.orderDAO.find_orders_by_buyer(user_id, status, page_size, first_result)
     
     return orders, total
+
+def admin_orders(buyer = None, seller = None, status = None, start_date = None, end_date = None, page = 1, page_size = 10):
+    first_result = (page - 1) * page_size
+    total = models.orderDAO.count_orders_by_admin(buyer, seller, status, start_date, end_date)
+    orders = models.orderDAO.find_orders_by_admin(buyer, seller, status, start_date, end_date, page_size, first_result)
+    
+    return orders, total
+    
+    
