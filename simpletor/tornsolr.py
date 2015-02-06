7658e25cf87315b32240077e8438dc47af650cc6
zhuhua <zhuhua@zhuhua-H61M-DS2>
2015-02-06 12:02:39 +0800

    Update Solr

--- simpletor/tornsolr.py
+++ simpletor/tornsolr.py
@@ -5,6 +5,7 @@ Created on Jan 23, 2015
 @author: zhuhua
 '''
 import pysolr
+import copy
 import settings
 
 class index:
@@ -15,8 +16,19 @@ class index:
     def __call__(self, method):
         def wrapper(*args, **kwds):
             obj = method(*args, **kwds)
+            obj_copy = copy.deepcopy(obj)
+            
+            if isinstance(obj_copy, dict):
+                keys = obj_copy.keys()
+                for k in keys:
+                    v = obj_copy[k]
+                    if isinstance(v, dict):
+                        for kk, vv in v.iteritems():
+                            obj_copy['%s_%s' % (k, kk)] = vv
+                        del obj_copy[k]
+                    
             solr = pysolr.Solr('%s/%s' % (settings.solr, self.core), timeout=10)
-            solr.add([obj])
+            solr.add([obj_copy])
             return obj
         return wrapper
     
