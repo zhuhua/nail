69185951aae354aebabd2c42861a5f6c3a247c10
lostsquirrel <songli1988@gmail.com>
2015-03-21 22:04:40 +0800

    evaluate count

--- sample/services.py
+++ sample/services.py
@@ -82,10 +82,13 @@ def get_sample(sample_id):
     
     counts = common_services.get_counts(sample_id, 'sample')
     
+    print 'counts', counts
     if len(counts) == 0:
         sample.counts = sample_count
+        print '0', sample.counts
     else:
         sample.counts = sample_count.update(counts)
+        print '1', sample.counts
         
     return sample
         
